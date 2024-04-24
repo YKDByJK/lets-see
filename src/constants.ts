2427452896998f153d23996de238cdd9be8783cf
YKDByJK <133152542+YKDByJK@users.noreply.github.com>
2024-04-24 16:55:52 -0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -58,7 +58,7 @@ export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
     mint: new PublicKey('BgFhv7WXJameaDeuwW5aSSGYyi6MpxE8yfyC2jYEssyy'),
     name: 'YonderKidDreams',
     symbol: 'YKD',
-    image: '',
+    image: '/Ykd token.png',
     baseWager: 1e6,
     decimals: 6,
     usdPrice: 0,
